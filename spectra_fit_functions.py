3a5f902366752bca7e1b979b38fb9c2b99b2b2c6
Jordan M. Young <54070169+Jordan-M-Young@users.noreply.github.com>
2019-08-19 16:48:31 -0400

    Update spectra_fit_functions.py

--- spectra_fit_functions.py
+++ spectra_fit_functions.py
@@ -13,8 +13,27 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
 import pandas as pd
 
-
+def num_name_gen(num_peaks):
+    """generates names for your parameter dictionary based 
+       on the number of peaks you're interested in"""
     
+    nums = []
+    names = []
+    
+    for i in range(num_peaks):
+        nums.append(int(0 + (5 * i)))
+        names.append('peak_number_' + str(i+1))
+        nums.append(int(1 + (5 * i)))
+        names.append('peak_position_' + str(i+1))
+        nums.append(int(2 + (5 * i)))
+        names.append('peak_intensity_' + str(i+1))
+        nums.append(int(3 + (5 * i)))
+        names.append('peak_fwhm_' + str(i+1))
+        nums.append(int(4 + (5 * i)))
+        names.append('peak_area_' + str(i+1))
+    
+    return names,nums
+
 def Param_Read(name,num_peaks=2):
     """opens .csv file of spectral parameters and unpacks data"""
     count = 0
@@ -25,20 +44,9 @@ def Param_Read(name,num_peaks=2):
         for row in reader:
             dictionary[str(count)] = row
             count += 1
-
-    nums = []
-    names = []
-    for i in range(num_peaks):
-      nums.append(int(0 + (5 * i)))
-      names.append('peak_number_' + str(i))
-      nums.append(int(1 + (5 * i)))
-      names.append('peak_position_' + str(i))
-      nums.append(int(2 + (5 * i)))
-      names.append('peak_intensity_' + str(i))
-      nums.append(int(3 + (5 * i)))
-      names.append('peak_fwhm_' + str(i))
-      nums.append(int(4 + (5 * i)))
-      names.append('peak_area_' + str(i))
+    
+    names,nums = num_name_gen(num_peaks)
+   
       
     nums.append(5 * num_peaks)
     names.append('index')
@@ -47,66 +55,23 @@ def Param_Read(name,num_peaks=2):
         element_list = []
         for element in dictionary[str(nums[i])]:
             element_list.append(float(element))
-            p_dic[names[i]] = element_list
+            params[names[i]] = element_list
 
     
   
-    return p_dic
+    return params
 
-def value_correction(params,choice=1,upperbound='none',lowerbound='none'):
-    """takes unpacked params, screens for bad data points and separates data
-    into two groups based on the Kd classifier parameter"""
-    
-    
-    
-    
-def mean(x):
-    """computes the mean value of a given array"""
-    
-    elements = len(x)
-    summ = sum(x)
-    if elements == 0:
-        a = 0
-    else:
-        a = summ/elements
-    return a
-
-
-    
-
-def avg_params(name,params):
-    """computes the average value for each spectral parameter"""
-    
-    dic1 = {}
-    
-
-    dic1['XD1'] = np.mean(params[0])
-    dic1['sig0'] = np.std(params[0])
-    dic1['WD1'] = np.mean(params[1])
-    dic1['sig1'] = np.std(params[1])
-    dic1['XG1'] = np.mean(params[2])
-    dic1['sig2'] = np.std(params[2])
-    dic1['WG1'] = np.mean(params[3])
-    dic1['sig3'] = np.std(params[3])
-    dic1['IDIG1'] = np.mean(params[4])
-    dic1['sig4'] = np.std(params[4])
-    dic1['KD1'] = np.mean(params[5])
-    dic1['sig5'] = np.std(params[5])
-    dic1['KG1'] = np.mean(params[6])
-    dic1['sig6'] = np.std(params[6])
-    dic1['DSLOPE1'] = np.mean(params[7])
-    dic1['sig7'] = np.std(params[7])
-    dic1['GSLOPE1'] = np.mean(params[8])
-    dic1['sig8'] = np.std(params[8])
-    dic1['T1'] = np.mean(params[9])
-    dic1['sig9'] = np.std(params[9])
    
-        
+    
+   
+def avg_params(params):
+    """computes the average value for each spectral parameter"""
+    spec_params = {}
+    for key, value in params.items()
+        spec_params[key] = np.mean(np.array(value))
+   
+    return spec_params
                       
      
-param_Total = pd.DataFrame(param_T,index=names,columns=keyys)
 
-#params_A.to_csv('A_TYPE_A_PARAMS.csv')
-#params_B.to_csv('B_TYPE_B_PARAMS.csv')
-#param_Total.to_csv('Total_PARAMS.csv')
 
